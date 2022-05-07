509d5c1b06de14c3c9b4a1c835d5d20d29683888
Susana Chang <samanta.chang@utec.edu.pe>
2022-05-06 23:15:46 -0500

    revision de dificultad de password

--- app.py
+++ app.py
@@ -69,15 +69,40 @@ def login():
 def register():
     return render_template('register.html')
 
+def check_password_difficulty(password_to_check):
+    with open("password_lists.txt", "r") as file:
+        for password in file:
+            if password.rstrip() == password_to_check:
+                return False
+    mayusc_amount = len([i for i in password_to_check if i.isupper()])
+    minusc_amount = len([i for i in password_to_check if i.islower()])
+    digit_amount = len([i for i in password_to_check if i.isdigit()])
+    special_amount = len([i for i in password_to_check if i in "!#$%&()=+-."])
+    password_length = True if len(password_to_check) >= 6 else False
+    if mayusc_amount and minusc_amount and digit_amount and special_amount and password_length:
+        return True
+    else:
+        return False
+
+
 @app.route('/register/create', methods=['POST', 'GET'])
 def register_create():
     error = False
     try:
         username = request.form.get("username", '')
         password = request.form.get("password", '')
-        user = User(username=username, password=password, role="user", dateCreated=func.now())
-        db.session.add(user)
-        db.session.commit()
+        password_check = request.form.get("password_check", '')
+        if password == password_check:
+            if check_password_difficulty(password) and len(username) > 0:
+                user = User(username=username, password=password, role="user", dateCreated=func.now())
+                db.session.add(user)
+                db.session.commit()
+            else:
+                error = True
+                db.session.rollback()
+        else:
+            error = True
+            db.session.rollback()
     except Exception as e:
         error = True
         print(e)
